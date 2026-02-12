8b42e532da8e6caed05a72b40b9b7559ef12984a
Abdelrahman Mohamed <87266656+Abdelrahman-95@users.noreply.github.com>
2026-02-12 22:40:47 +0200

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+               
+                Console.WriteLine("Input days attended: ");
+
+              
+                int days = Convert.ToInt32(Console.ReadLine());
+
+                if (days < 10)
+                {
+                    Console.WriteLine("Not Eligible");
+                }
+                else if (days >= 10 && days <= 19)
+                {
+                    Console.WriteLine("Eligible");
+                }
+                else 
+                {
+                    Console.WriteLine("Excellent Attendance");
+                }
+
+                
+                Console.ReadKey();
+         
+        }
+    }
+}
